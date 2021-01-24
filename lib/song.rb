621ba6eef7af59a6d07c8ee3aed0fc9b6c776876
Sara Nolen <nolensara1@gmail.com>
2021-01-24 18:05:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,9 +28,9 @@ class Song
 
   def self.genre_count
     genre_count = {}
-    @@genres.each do |genre| 
-      if genre_count[genre] 
+    @@genres.each do |genre|
+      if genre_count[genre]
         genre_count[genre] += 1
-      else 
+      else
         genre_count[genre] = 0
 end
