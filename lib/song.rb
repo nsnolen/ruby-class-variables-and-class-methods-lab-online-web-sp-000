d7f00553b6be3e2120340e41a62f68d4ebccb6bd
Sara Nolen <nolensara1@gmail.com>
2021-01-24 17:47:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,9 @@
 class Song
-  @@count = 0
-#  @@artists_count = 0
-#  @@song.genre_count = 0
-
-  def initialize
-    @@count += 0
-  end
-
-  def self.count
-    @@count
+  attr_accessor :name, :artist, :genre
+  @@count
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = {}
+    @genre = {}  
   end
 end
